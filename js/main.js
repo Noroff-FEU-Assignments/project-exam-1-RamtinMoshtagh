614d713da713339a085e81889076e141b3fd6d4e
RamtinMoshtagh <95631899+RamtinMoshtagh@users.noreply.github.com>
2023-02-08 07:39:44 +0100

    Swiper slide
    creating the swiper for the blog posts. clicke or swipe to see other posts.

--- js/main.js
+++ js/main.js
@@ -27,3 +27,23 @@ formCloseBtn.addEventListener('click', () => searchFormContainer.classList.remov
 window.addEventListener('keyup', event => {
     if(event.key ==='Esacape') searchFormContainer.classList.remove('activated');
 });
+const swiper = new Swiper(".swiper",{
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    pagination: {
+        el:".swiper-pagination"
+    },
+    breakpoints:{
+        700: {
+            slidesPerView: 2  
+        },
+        1200: {
+            slidesPerView: 3  
+        }
+
+    }
+});
